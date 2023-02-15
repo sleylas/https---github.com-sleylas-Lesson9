cedc0ee89f84de6ba93d60f63b08fc03b3dbb4b1
Leyla Sadirkhanly <slsedrik@gmail.com>
2023-02-15 19:03:26 +0400

    ADED

--- square.test.js
+++ square.test.js
@@ -1,8 +1,14 @@
 const square = require("./square");
 describe("Testing of square expression", () => {
-    it("Disc is more than 0", () => {
-        result = [4, -1];
-        expect(square(1, -3, -4)).toEqual(result);
+            it("Disc is more than nulya", () => {
+                result = [4, -1];
+                expect(square(1, -3, -4)).toEqual(result);
 
-    });
-});
+            });
+            it("Disc is equal nulya", () => {
+                expect(square(9, 6, 1)).toEqual(-27);
+
+            });
+            it("Disc is more than nulya", () => {
+                expect(square(9, 6, 2)).toEqual("no roots");
+            });
