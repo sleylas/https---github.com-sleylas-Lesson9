5e41dd23d10339898a3cb52460b17d10d81a6d2f
Leyla Sadirkhanly <slsedrik@gmail.com>
2023-02-17 21:36:36 +0400

    Coverage

--- check.js
+++ check.js
@@ -2,7 +2,12 @@ const sum = (a, b) => {
     let bonus;
     const sum = a + b;
     debugger; // Stop there and shows sum (a+b)
-    sum > 60 ? (bonus = 50) : (bonus = sum);
+    sum > 60 ? (bonus = 100) : (bonus = sum);
+    // if (sum < 60) {
+    //     (bonus = 50);
+    // } else {
+    //     (bonus = sum);
+    // }
     debugger; //Stop there and shows if sum is greater than 50 then bonus equals 50 else bonus equals sum of (a+b)
     return bonus;
 };
--- check.test.js
+++ check.test.js
@@ -3,13 +3,15 @@ describe("Show testing sum", () => {
     it("Testing if sum is 60", () => {
         expect(sum(30, 30)).toBe(60);
     });
-    it("Testing if sum is equal 60", () => {
-        expect(sum(30, 30)).toEqual(60);
+    it("Testing if sum is greater than 99", () => {
+        const result = sum(70, 30);
+        expect(sum(70, 30)).toBe(100);
+        // expect(sum(30, 30)).toEqual(60);
     });
-    it("Testing if sum is greater than 60", () => {
-        expect(sum(30, 30)).toBeGreaterThan(59);
-    });
-    it("Testing if sum less than 61", () => {
-        expect(sum(30, 30)).toBeLessThan(61);
-    });
+    // it("Testing if sum is greater than 60", () => {
+    //     expect(sum(30, 30)).toBeGreaterThan(59);
+    // });
+    // it("Testing if sum less than 61", () => {
+    //     expect(sum(30, 30)).toBeLessThan(61);
+    // });
 });
